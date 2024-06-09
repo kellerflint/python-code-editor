e1e3cf2d26d00eac893ace0f7bd22160648e5807
Keller Flint <kflint0068@gmail.com>
2024-06-09 15:53:51 -0700

    refactor

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+
+// MongoDB connection
+/*
+const mongoose = require('mongoose');
+
+const dbUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pf0frdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
+*/
